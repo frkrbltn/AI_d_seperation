0554116c1f71b247725f9226b15b216189501246
fkarabu <fkarabu@ncsu.edu>
2023-11-20 10:16:22 -0500

    [failed] failed in 2 test cases

--- CSC411_ProblemSet06/src/edu/ncsu/csc411/ps06/dseperation/DSeparation.java
+++ CSC411_ProblemSet06/src/edu/ncsu/csc411/ps06/dseperation/DSeparation.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import edu.ncsu.csc411.ps06.graph.Graph;
 import edu.ncsu.csc411.ps06.graph.Path;
 import edu.ncsu.csc411.ps06.graph.Vertex;
+import edu.ncsu.csc411.ps06.util.EdgeType;
+// import Edge java file
+import edu.ncsu.csc411.ps06.graph.Edge;
 
 /**
  * Problem Set 06 - For this Problem Set you will be implementing the D-Separation
@@ -38,7 +41,58 @@ public class DSeparation<V extends Comparable<V>> {
    */
   public boolean dSeparation(ArrayList<Path> paths, ArrayList<Vertex<V>> evidence) {
     // Feel free to utilize the Graph object's getEdge and getEdgeType for this PS.
-    return false;
+    // Create a helper method that returns a triple
+    // given a path and the graph.
+
+    // Iterate thorough path
+    for (Path path : paths) {
+      // Iterate through each vertex in the path
+      if (!independent(path, evidence)) {
+        return false;
+      }
+    }
+    return true;
   }
+
+  private boolean independent(Path path, ArrayList<Vertex<V>> evidence) {
+    ArrayList<Vertex> vertices = path.getVertices();
+    for (int i = 0; i < vertices.size() - 2; i++) {
+        Vertex<V> A = vertices.get(i);
+        Vertex<V> B = vertices.get(i + 1);
+        Vertex<V> C = vertices.get(i + 2);
+        if (!active(A, B, C, evidence)) {
+            return false; // If any triple is active, the path is active
+        }
+    }
+    return true; // If no triples are active, the path is inactive
+  }
+
+  private boolean active(Vertex<V> A, Vertex<V> B, Vertex<V> C, ArrayList<Vertex<V>> evidence) {
+    EdgeType edgeAB = graph.getEdge(A, B).getEdgeType();
+    EdgeType edgeBC = graph.getEdge(B, C).getEdgeType();
+
+    if (edgeAB == EdgeType.TO && edgeBC == EdgeType.TO) {
+        // Chain A -> B -> C
+        return evidence.contains(B);
+    } else if (edgeAB == EdgeType.FROM && edgeBC == EdgeType.FROM) {
+        // Fork A <- B -> C
+        return evidence.contains(B);
+    } else if ((edgeAB == EdgeType.TO && edgeBC == EdgeType.FROM) || (edgeAB == EdgeType.FROM && edgeBC == EdgeType.TO)) {
+        // Collider A -> B <- C or A <- B -> C
+        return inactive(B, evidence);
+    }
+    return false;
+}
+
+private boolean inactive(Vertex<V> collider, ArrayList<Vertex<V>> evidence) {
+    // Collider is inactive if neither the collider nor its descendants are in evidence
+    if (evidence.contains(collider)) return false;
+
+    for (Vertex<V> descendent : collider.neighbors) {
+        if (evidence.contains(descendent)) return false;
+    }
+
+    return true;
+}
 
 }
